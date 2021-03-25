9cd63541e00907d2196dc28cbb0fd0f6f53b130d
ldfracao <ldfracao@protonmail.com>
2021-03-25 02:14:55 -0300

    finished cash.c

--- week01/cash-credit/cash.c
+++ week01/cash-credit/cash.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
+#include <math.h>
 
 int main (void) 
 {
-    float change;
-    char ch;
+    float input;
+    int change;
+    int rest;
+    int totalCoins;
+    char *ch;
     char buffer[50];
 
-    // input filter, doesn't filter negative numbers yet
+    totalCoins = 0;
+    rest = 0;
+
+    // input filter
     do 
     {
-        printf("How much change: ");
-        if (fgets(buffer, sizeof buffer, stdin) == NULL) 
+        printf("How much change?\n");
+        ch = fgets(buffer, sizeof buffer, stdin);
+        if (ch == NULL) 
         {
-            continue;
-        }  
-    } while (sscanf(buffer, "%f %c", &change, &ch) != 1);
+            break;
+        }
+    } while (sscanf(buffer, "%f %c", &input, ch) != 1 || input < 0);
 
-    // continue logic
+    input *= 100;
+    change = round(input);
+
+    while (change - rest >= 25)
+    {
+        rest += 25;
+        totalCoins++;
+    }
+    while (change - rest >= 10)
+    {
+        rest += 10;
+        totalCoins++;
+    }
+    while (change - rest >= 5)
+    {
+        rest += 5;
+        totalCoins++;
+    }
+    while (change - rest >= 1)
+    {
+        rest += 1;
+        totalCoins++;
+    }
+    printf("%i\n", totalCoins);
 }

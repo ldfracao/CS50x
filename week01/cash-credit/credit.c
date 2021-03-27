79ce4e5e77ad4e8d58699ba239e7321c2419141f
ldfracao <ldfracao@protonmail.com>
2021-03-26 21:16:53 -0300

    implement reading every other characters starting from second to last

--- week01/cash-credit/credit.c
+++ week01/cash-credit/credit.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //helper function to check if an input has only numbers
 int     input_numbers(char *str)
@@ -8,8 +9,7 @@ int     input_numbers(char *str)
 
     i = 0;
 
-    // fgets terminator character is a new line feed, 10 in the ASCII table
-    while (str[i] != 10)
+    while (str[i] != '\n')
     {
         if (str[i] >= '0' && str[i] <= '9')
             i++;
@@ -22,13 +22,44 @@ int     input_numbers(char *str)
 int     main (void)
 {
 
-    char *buffer = malloc(16 * sizeof(char));
+    char *user_input = malloc(16 * sizeof(char));
+    int *multiplier = malloc(8 * sizeof(int));
+
+    int j;
+    int k;
+
+    k = 0;
 
     do
     {
         printf("Enter a credit card number: ");
-        fgets(buffer, 17, stdin);
-    } while (input_numbers(buffer) == 1);
+        fgets(user_input, 17, stdin);
+    } while (input_numbers(user_input) == 1);
+
+    j = strlen(user_input);
+    printf("%i\n", j);
+
+    while (user_input[k] != '\0')
+    {
+        multiplier[k] = user_input[j - 3];
+        j -= 2;
+        k++;
+    }
+    int m = 0;
+
+    while (multiplier[m] != '\0')
+    {
+        printf("%c\n", multiplier[m]);
+        m++;
+    }
     
-    free(buffer);
+    // printf("%c %c\n", user_input[j - 3], multiplier[k]);
+
+    // while (j >= 0)
+    // {
+    //     // multiplier[k] = user_input[j - 3] * 2;
+    //     j -= 2;
+    //     k++;
+    // }
+    free(user_input);
 }

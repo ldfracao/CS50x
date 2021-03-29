5a6dd2d0a7278adc8b048c25eabebd7e41eb2a4a
ldfracao <ldfracao@protonmail.com>
2021-03-29 20:26:12 -0300

    initial caeser.c commit

---
+++ week02/caesar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    int key;
+    if(argc != 2)
+    {
+        printf("Please provide the cipherkey (integer)");
+        return 1;
+    }
+}

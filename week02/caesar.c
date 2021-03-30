def335f50c9b01ea57659e00db8200ff227a535a
ldfracao <ldfracao@protonmail.com>
2021-03-30 11:42:58 -0300

    add converter from char to int

--- week02/caesar.c
+++ week02/caesar.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 int main (int argc, char *argv[])
 {
     char *plaintext = malloc(20 * sizeof(char));
-    if (plaintext != NULL)
+    int key = 0;
+    char *endp = NULL;
+    
+    if (plaintext != NULL) //checkes for allocation errors
     {
+        // check if user provided only command line argument
         if(argc != 2)
         {
             printf("Usage: ./caesar key\n");
             return 1;
         }
-
+        // check if that command line argument contains only numbers
         for (int i = 0; argv[1][i] != '\0'; i++)
         {
             if (argv[1][i] >= '0' && argv[1][i] <= '9')
+            {
                 continue;
+                
+            }
             else
             {
                 printf("Usage: ./caesar key\n");
                 return 1;
             }
         }
+        //converts key chars into an int
+        key = strtoimax(argv[1], &endp, 10);
+
         printf("plaintext: ");
         fgets(plaintext, 20, stdin);
+        printf("ciphertext: ");
+        printf("\n");
     }
+    else
+        return 1;
+
     free(plaintext);
     return 0;
 }

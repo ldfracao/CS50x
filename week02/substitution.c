8f3c6ef1ad48c49c82b913c874b3b8546c26a06f
ldfracao <ldfracao@protonmail.com>
2021-03-31 14:21:23 -0300

    finish substitution.c

--- week02/substitution.c
+++ week02/substitution.c
@@ -5,6 +5,7 @@
 int     main (int argc, char *argv[])
 {
     char *plaintext = malloc(200 * sizeof(char));
+    char lowermap[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     //checks for allocation errors
     if (plaintext != NULL)
@@ -15,11 +16,15 @@ int     main (int argc, char *argv[])
             printf("Usage: ./substitution key\n");
             return 1;
         }
+
+        // checks if key contains 26 characters
         else if (strlen(argv[1]) != 26)
         {
             printf("Key must contain 26 characters.\n");
             return 1;
         }
+
+        // checks if characters are only alphabetical
         for (int i = 0; argv[1][i]; i++)
         {
             if((argv[1][i] >= 'A' && argv[1][i] <= 'Z') || (argv[1][i] >= 'a' && argv[1][i] <= 'z'))
@@ -30,11 +35,13 @@ int     main (int argc, char *argv[])
                 return 1;
             }
         }
+
+        // checks if key contains any repeated characters
         for (int i = 0; argv[1][i] != '\0'; i++)
         {
-            for (int j = 0; argv[1][j] != '\0'; j++)
+            for (int j = i; argv[1][j] != '\0'; j++)
             {
-                if (argv[1][i] == argv[1][j])
+                if (argv[1][i] == argv[1][j + 1])
                 {
                     printf("Key must not contain repeated characters.\n");
                     return 1;
@@ -46,6 +53,38 @@ int     main (int argc, char *argv[])
         fgets(plaintext, 200, stdin);
         printf("ciphertext: ");
 
+        // cipher algorithm
+        for (int i = 0; plaintext[i] != '\0'; i++)
+        {
+            if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
+            {
+                for (int j = 0; lowermap[j] != '\0'; j++)
+                {
+                    if (plaintext[i] == lowermap[j])
+                    {
+                        plaintext[i] = argv[1][j];
+                        break;
+                    }
+                }
+                printf("%c", plaintext[i]);
+            }
+            else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
+            {
+                for (int j = 0; lowermap[j] != '\0'; j++)
+                {
+                    if (plaintext[i] == lowermap[j] - 32)
+                    {
+                        plaintext[i] = argv[1][j] - 32; //- 32 converts lower case into uppercase
+                        break;
+                    }
+                }
+                printf("%c", plaintext[i]);
+            }
+            else
+            {
+                printf("%c", plaintext[i]);
+            }
+        }
     }
     else
     {

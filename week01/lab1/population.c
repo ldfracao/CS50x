56fbbc111fe864be1b3381994f58e1da936a9115
ldfracao <ldfracao@protonmail.com>
2021-04-13 13:59:31 -0300

    finish population.c

--- week01/lab1/population.c
+++ week01/lab1/population.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <math.h>
 
 // helper function to check if an input has only numbers characters
 int     input_numbers(char *str)
@@ -22,8 +23,8 @@ int     input_numbers(char *str)
 int main(void)
 {
     // start and end size variables
-    char *start = malloc(10 * sizeof(char));
-    char *end = malloc(10 * sizeof(char));
+    char *start = malloc(100 * sizeof(char));
+    char *end = malloc(100 * sizeof(char));
 
     // helper pointer used in strtoimax function
     char *endp = NULL;
@@ -32,11 +33,17 @@ int main(void)
     int start_conversion;
     int end_conversion;
 
+    int years; // number of years taken
+    int born; // number of llamas born in a year
+    int died; // number of llamas that died in a year
+
+    years = 0;
+
     // Prompt for start size and keep asking for input until it contains only numbers
     do
     {
         printf("Start size: ");
-        fgets(start, 10, stdin);
+        fgets(start, 25, stdin);
         start_conversion = strtoimax(start, &endp, 10);
 
     } while (input_numbers(start) == 1 || start_conversion < 9);
@@ -45,12 +52,24 @@ int main(void)
     do
     {
         printf("End size: ");
-        fgets(end, 10, stdin);
+        fgets(end, 100, stdin);
         end_conversion = strtoimax(end, &endp, 10);
 
     } while (input_numbers(end) == 1 || end_conversion < start_conversion);
     
-    // TODO: Calculate number of years until we reach threshold
+    // Calculate number of years until we reach threshold
+    while (start_conversion < end_conversion)
+    {
+        born = round(start_conversion / 3);
+        died = round(start_conversion / 4);
+        start_conversion = start_conversion + born - died;
+        years++;
+    }
 
-    // TODO: Print number of years
+    // free malloced variables
+    free(start);
+    free(end);
+
+    // Print number of years
+    printf("%i\n", years);
 }
